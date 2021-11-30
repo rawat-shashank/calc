9754eda1f8403456162b3d0c827dc069a87e359e
rawat-shashank <shashank17nov@gmail.com>
2021-11-30 10:55:53 +0000

    Deploying to gh-pages from @ rawat-shashank/calc@2097f9bf39eb9a1402233e0ac1232830ce15d483 🚀

---
+++ _next/static/_RAKol_xDnhDTBQJLFmvL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c686dbb40f61045e.css","static/chunks/pages/index-d3c7a84a5c6a4944.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
